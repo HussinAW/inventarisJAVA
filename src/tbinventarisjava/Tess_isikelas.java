db2b8f8f6cb5f76891f02d9fda90bf2463c77935
Dnurmasari <nurmasari.np@gmail.com>
2015-04-24 20:23:16 +0700

    Merge origin/master

--- src/tbinventarisjava/Tess_isikelas.java
+++ src/tbinventarisjava/Tess_isikelas.java
@@ -36,7 +36,7 @@ Isikelas baru = new Isikelas();
      System.out.print(" : " +baru.getPosisi_stopkontak() + " Tidak sesuai\n");    
     }
    
-   System.out.println("=================================================================================================");
+   System.out.println("=================================================================================================\n");
     
    }
   public void analisis_LCD(){
@@ -46,10 +46,10 @@ Isikelas baru = new Isikelas();
      System.out.print(" => " + baru.getJumlah_kabel_LCD() + " Sesuai");   
     }
     else{
-    System.out.print(" => " + baru.getJumlah_kabel_LCD() + " Tidak sesuai\n\n");    
+    System.out.print(" => " + baru.getJumlah_kabel_LCD() + " Tidak sesuai\n");    
     }
-      System.out.println("KONDISI KABEL LCD PILIH berfungsi ATAU tidak berfungsi");
-      System.out.println("PENULISAN HARUS SESUAI DENGAN PILIHAN\n");
+      System.out.println("+*KONDISI KABEL LCD PILIH berfungsi ATAU lainnya*+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n");
       System.out.println("Kondisi kabel LCD = ");
       baru.setKondisi_kabel_LCD(input.next());
     if(baru.getKondisi_kabel_LCD().equals("berfungsi")){
@@ -58,8 +58,8 @@ Isikelas baru = new Isikelas();
     else{
     System.out.print(" => " + baru.getKondisi_kabel_LCD()+ " Tidak sesuai\n\n");    
     }
-  System.out.println("POSISI KABEL LCD PILIH ANTARA dekatdosen ATAU lainnya");
-  System.out.println("");
+     System.out.println("+*POSISI KABEL LCD PILIH dekatdosen ATAU lainnya*+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n");
       System.out.println("Masukkan posisi kabel LCD = ");
       baru.setPosisi_kabel_LCD(input.next());
       if(baru.getPosisi_kabel_LCD().equals("dekatdosen")){
@@ -68,7 +68,7 @@ Isikelas baru = new Isikelas();
     else{
         System.out.println(" => " + baru.getPosisi_kabel_LCD()+" Tidak sesuai ");
     }
-    System.out.println("==================================================================================================");
+   System.out.println("=================================================================================================\n");
   }     
   public void analisis_lampu(){
       System.out.println("Masukkan jumlah lampu = ");
@@ -84,78 +84,84 @@ Isikelas baru = new Isikelas();
       baru.setKondisi_lampu(input.nextInt());
     if(baru.getKondisi_lampu()==18){
      System.out.print(" => " + baru.getKondisi_lampu()+" Sesuai ");   
-    }
-    
-    
+    } 
     else{
      System.out.print(" => " + baru.getKondisi_lampu()+" Tidak sesuai \n\n");    
     }
-      System.out.println("Masukkan posisi lampu = ");
+    System.out.println("+*TULIS POSISI LAMPU ANTARA DI atapruangan ATAU lainnya*+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n");
+    System.out.println("Masukkan posisi lampu = ");
       baru.setPosisi_lampu(input.next());
-      if(baru.getPosisi_lampu().equals("Atap Ruangan")){
-    System.out.println("Posisi Lampu   = " + baru.getPosisi_lampu()+ "Sesuai"); 
+      if(baru.getPosisi_lampu().equals("atapruangan")){
+    System.out.print(" => " + baru.getPosisi_lampu()+ "Sesuai"); 
     }
     else{
-    System.out.println("Posisi Lampu   = " + baru.getPosisi_lampu()+ "tidak sesuai");     
+    System.out.print(" => " + baru.getPosisi_lampu()+ "tidak sesuai");     
     }
-      
+   System.out.println("=================================================================================================\n");
   }
   public void analisis_kipas_angin(){
       System.out.println("Masukkan jumlah kipas angin = ");
       baru.setJumlah_kipasangin(input.nextInt());
       if(baru.getJumlah_kipasangin()>=2){
-     System.out.println("Jumlah Kipas angin = " + baru.getJumlah_kipasangin()+"Sesuai");   
+     System.out.print(" => " + baru.getJumlah_kipasangin()+"Sesuai");   
     }
     else{
-     System.out.println("Jumlah Kipas angin = " + baru.getJumlah_kipasangin()+"Tidak sesuai");    
+     System.out.print(" => " + baru.getJumlah_kipasangin()+"Tidak sesuai");    
     }
        System.out.println("Masukkan kondisi kipas angin = ");
       baru.setKondisi_kipasangin(input.nextInt());
       if(baru.getKondisi_kipasangin()==2){
-    System.out.println("Kondisi Kipas angin yang baik = " + baru.getKondisi_kipasangin()+"Sesuai");    
+    System.out.print(" => " + baru.getKondisi_kipasangin()+"Sesuai");    
     }
     else{
-    System.out.println("Kondisi Kipas angin yang baik = " + baru.getKondisi_kipasangin()+"Tidak sesuai");   
+    System.out.print(" => " + baru.getKondisi_kipasangin()+"Tidak sesuai\n\n");   
     }
+     System.out.println("+*TULIS POSISI KIPAS ANGIN ANTARA DI atapruangan ATAU lainnya*+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n"); 
       System.out.println("Masukkan posisi kipas angin = ");
       baru.setPosisi_kipasangin(input.next());
-      if(baru.getPosisi_kipasangin().equals("Atap ruangan")){
-     System.out.println("Posisi Kipas angin = " + baru.getPosisi_kipasangin()+"Sesuai");   
+      if(baru.getPosisi_kipasangin().equals("atapruangan")){
+     System.out.print(" => " + baru.getPosisi_kipasangin()+"Sesuai");   
     }
     else{
-        System.out.println("Posisi Kipas angin = " + baru.getPosisi_kipasangin()+"Tidak sesuai");
+        System.out.println(" => " + baru.getPosisi_kipasangin()+"Tidak sesuai");
     }
-     
+   System.out.println("=================================================================================================\n");  
   }
   public void analisis_AC(){
       System.out.println("Masukkan jumlah Ac = ");
       baru.setJumlah_Ac(input.nextInt());
       if(baru.getJumlah_Ac()>=1){
-     System.out.println("Jumlah Ac = " + baru.getJumlah_Ac()+"Sesuai");    
+     System.out.print(" => " + baru.getJumlah_Ac()+"Sesuai");    
     }
     else{
-    System.out.println("Jumlah Ac = " + baru.getJumlah_Ac()+"Tidak sesuai");     
+    System.out.println(" => " + baru.getJumlah_Ac()+"Tidak sesuai\n");     
     }
+      System.out.println("+*TULIS KONDISI AC ANTARA baik  ATAU buruk*+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n"); 
       System.out.println("Masukkan kondisi Ac = ");
       baru.setPosisi_Ac(input.next());
       if(baru.getKondisi_Ac().equals("baik")){
-      System.out.println("Kondisi Ac = " + baru.getKondisi_Ac()+"Sesuai");   
+      System.out.print( " => " + baru.getKondisi_Ac()+"Sesuai");   
     }
     else{
-        System.out.println("Kondisi Ac = " + baru.getKondisi_Ac()+"Tidak sesuai");
+        System.out.print(" => " + baru.getKondisi_Ac()+"Tidak sesuai");
     }
+     System.out.println("+*TULIS POSISI ANTARA didepan  ATAU disamping ATAU lainnya *+");
+      System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n");   
       System.out.println("Masukkan posisi Ac = ");
       baru.setPosisi_Ac(input.next());
-      if(baru.getPosisi_Ac().equals("Didepan")){
-    System.out.println("Posisi Ac = " + baru.getPosisi_Ac()+"Sesuai");
+      if(baru.getPosisi_Ac().equals("didepan")){
+    System.out.print(" => " + baru.getPosisi_Ac()+"Sesuai");
     }
-    else if(baru.getPosisi_Ac().equals("Disamping")){
-    System.out.println("Posisi Ac = " + baru.getPosisi_Ac()+"Sesuai");
+    else if(baru.getPosisi_Ac().equals("disamping")){
+    System.out.print(" => " + baru.getPosisi_Ac()+"Sesuai");
     }
     else{
-    System.out.println("Posisi Ac = " + baru.getPosisi_Ac()+"Tidak sesuai");
+    System.out.print(" => " + baru.getPosisi_Ac()+"Tidak sesuai");
     }
-      
+  System.out.println("=================================================================================================\n");    
   }
  public void analisis_internet(){
      System.out.println("Masukkan SSID = ");
@@ -166,6 +172,7 @@ Isikelas baru = new Isikelas();
     else{
     System.out.println("SSID = " + baru.getSSID() +"tidak sesuai"+baru.getBandwith());       
     }
+    System.out.println("=================================================================================================\n");
  }
 //public void analisis_Bandwith(){
     //System.out.println("Bandwith = ");
@@ -175,25 +182,27 @@ public void analisis_CCTV(){
 System.out.println("Masukkan jumlah CCTV =  ");
 baru.setJumlah_CCTV(input.nextInt());
 if(baru.getJumlah_CCTV()==2){
-    System.out.println("Jumlah CCTV = " + baru.getJumlah_CCTV()+"Sesuai");    
+    System.out.print(" => " + baru.getJumlah_CCTV()+"Sesuai");    
     }
     else{
-        System.out.println("Jumlah CCTV = " + baru.getJumlah_CCTV()+"Tidak sesuai");
+        System.out.print(" => " + baru.getJumlah_CCTV()+"Tidak sesuai");
     }
-System.out.println("Masukkan kondisi CCTV = ");
+System.out.println("Jumlah CCTV dalam keadaan baik = ");
 baru.setKondisi_CCTV(input.nextInt());
 if(baru.getKondisi_CCTV()==2){
-     System.out.println("Kondisi CCTV baik = " + baru.getKondisi_CCTV()+"sesuai");   
+     System.out.print(" => " + baru.getKondisi_CCTV()+"sesuai");   
     }
     else{
-    System.out.println("Kondisi CCTV = " + baru.getKondisi_CCTV() + "Tidak sesuai");
+    System.out.print(" => " + baru.getKondisi_CCTV() + "Tidak sesuai");
     }
+ System.out.println("+*TULIS POSISI CCTV ANTARA didepan  ATAU dibelakang ATAU lainnya *+");
+ System.out.println("+*PENULISAN HARUS SESUAI DENGAN PILIHAN*+\n");   
 System.out.println("Masukkan posisi CCTV = ");
 baru.setPosisi_CCTV(input.next());
-if(baru.getPosisi_CCTV().equals("Di depan")){
+if(baru.getPosisi_CCTV().equals("didepan")){
     System.out.println("Posisi CCTV = " + baru.getPosisi_CCTV()+"Sesuai");   
     }
-    else if(baru.getPosisi_CCTV().equals("Di belakang")){
+    else if(baru.getPosisi_CCTV().equals("dibelakang")){
     System.out.println("Posisi CCTV = " + baru.getPosisi_CCTV()+"Sesuai");    
     }
     else{
