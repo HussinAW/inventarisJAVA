62cdc503b721ad8002e57f8bd033c81a47816e28
HussinAW <hussin_agung@yahoo.com>
2015-04-26 21:01:22 +0700

    commit baru

--- src/tbinventarisjava/Pemanggilan.java
+++ src/tbinventarisjava/Pemanggilan.java
@@ -1,8 +1,10 @@
 package tbinventarisjava;
 public class Pemanggilan {
  public static void main(String[]Args){
-       Tess_isikelas OA = new Tess_isikelas();
+     Isikelas sur = new Isikelas(); //biar ditambah Suryanto . wooooey di teruskan// 
+     Tess_isikelas OA = new Tess_isikelas();
        kondisi_kelastest jaya=new kondisi_kelastest();
+       
        
        
        OA.analisis_kelistrikan();
@@ -12,7 +14,23 @@ public class Pemanggilan {
        OA.analisis_AC();
        OA.analisis_internet();
        OA.analisis_CCTV();
-        
+        jaya.Analisis_kebersihanlantai();
+        jaya.Analisis_kebersihandinding();
+        jaya.Analisis_kebersihanatap();
+        jaya.Analisis_kebersihanjendela();
+        jaya.Analisis_sirkulasiudara();
+        jaya.Analisis_pencahayaan();
+        jaya.Analisis_kelembapan();
+        jaya.Analisis_suhu();
+        jaya.Analisis_kebisingan();
+        jaya.Analisis_bau();
+        jaya.Analisis_kebocoran();
+        jaya.Analisis_kerusakan();
+        jaya.Analisis_aus();
+        jaya.Analisis_kekokohan();
+        jaya.Analisis_kuncipintu();
+        jaya.Analisis_kuncijendela();
+        jaya.Analisis_bahaya();
         
 }
 }
