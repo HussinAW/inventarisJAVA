33480ece25d006c2f61e6b86981aa6ca3d512bf1
HussinAW <hussin_agung@yahoo.com>
2015-04-26 19:30:55 +0700

    commit baru

--- src/tbinventarisjava/Pemanggilan.java
+++ src/tbinventarisjava/Pemanggilan.java
@@ -2,6 +2,9 @@ package tbinventarisjava;
 public class Pemanggilan {
  public static void main(String[]Args){
        Tess_isikelas OA = new Tess_isikelas();
+       kondisi_kelastest jaya=new kondisi_kelastest();
+       
+       
        OA.analisis_kelistrikan();
        OA.analisis_LCD();
        OA.analisis_lampu();
@@ -9,6 +12,7 @@ public class Pemanggilan {
        OA.analisis_AC();
        OA.analisis_internet();
        OA.analisis_CCTV();
-              
-  }    
+        
+        
+}
 }
--- src/tbinventarisjava/kondisi_kelastest.java
+++ src/tbinventarisjava/kondisi_kelastest.java
@@ -1,42 +1,18 @@
 package tbinventarisjava;
-
 import java.util.Scanner;
-
-
 public class kondisi_kelastest {
-
-
-    static Scanner input;
-    static Kondisi_kelas jay;
+    Kondisi_kelas jay = new Kondisi_kelas();
+       Scanner input = new Scanner(System.in);
     
-    public static void main(String[] args) {
-        input = new Scanner(System.in);
-        jay = new Kondisi_kelas();
-     
-    }
-       
-        public void analisis_lingkungan_ruang_kelas(){
-            System.out.println("masukan kondisi lantai(bersih/kotor)=");
-            jay.setKondisi_lantai(input.next());
-            System.out.println("masukan kondisi dinding(bersih/kotor)=");
-            jay.setKondisi_dinding(input.next());
-            System.out.println("masukan kondisi atap(bersih/kotor)=");
-            jay.setKondisi_lantai(input.next());
-            System.out.println("masukan kondisi pintu(bersih/kotor)=");
-            jay.setKondisi_pintu(input.next());
-              System.out.println("masukan kondisi jendela(bersih/kotor)=");
-            jay.setKondisi_jendela(input.next());
-        
-    }
-        public void kebersihan_ruang_kelas(){
-            System.out.println("masukan kondisi sirkulasi udara(baik/buruk)=");
-            jay.setKondisi_pintu(input.next());
-            System.out.println("masukan kondisi pencahayaan (dalam satuan lux)=");
-            jay.setKondisi_pencahayaan(input.nextInt());
-            System.out.println("masukan kondisi kelembapan(dalam satuan fanrenheit)=");
-            jay.setKondisi_kelembapan(input.nextInt());
-            System.out.println("masukan kondisi suhu");
-        }
-    }
-
-    
+          public void Analisis_kebersihan(){
+              System.out.print("masukkan kondisi lantai=");
+              jay.setKondisi_lantai(input.next());
+              
+              if(jay.getKondisi_lantai().equals("bersih")){
+         System.out.print(" => " + jay.Kondisi_lantai() + " Sesuai");   
+            }
+            else{
+            System.out.print(" => " + jay.getKondisi_lantai() + " Tidak sesuai\n");  
+              }         
+          }
+}
