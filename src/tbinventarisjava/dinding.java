bc0b3c2f8026604e3855509c1c4799296027752d
HussinAW <hussin_agung@yahoo.com>
2015-05-05 09:14:59 +0700

    commit baru

---
+++ src/tbinventarisjava/dinding.java
@@ -0,0 +1,9 @@
+package TBinventarisJAVA;
+public class dinding extends Kebersihan_ruang{
+    public void dinding(){  
+        System.out.println("dinding");
+        input();
+        show();
+        
+    }
+}
