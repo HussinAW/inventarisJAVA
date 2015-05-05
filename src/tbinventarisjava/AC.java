b46849135c67d1f8e128ec5f87ccf3c1d33d9f2e
Dnurmasari <nurmasari.np@gmail.com>
2015-05-05 10:17:22 +0700

    Merge origin/master

---
+++ src/tbinventarisjava/AC.java
@@ -0,0 +1,4 @@
+package tbinventarisjava;
+public class AC extends Elektronik{
+    
+}
---
+++ src/tbinventarisjava/LCD.java
@@ -0,0 +1,14 @@
+package tbinventarisjava;
+public class LCD extends Elektronik{
+public void LCD(){
+    setStandarJumlah(1);
+    setStandar_baik(1);
+    setStandarPosisi("dekatdosen");
+    System.out.println("Nama Barang: LCD");
+        input();
+        System.out.println("**********************************************");
+        show();
+    
+    
+}    
+}
