fadf5a75880d1c01f1fb4c25c8d3659ccdb357a6
SeptyaAndiSuryanto <bendox28@gmail.com>
2015-05-05 09:10:23 +0700

    commit kipas angin(induk elektronik)

---
+++ src/tbinventarisjava/Kipasangin.java
@@ -0,0 +1,15 @@
+package TBinventarisJAVA;
+public class Kipasangin extends Elektronik {
+    public void Kipasangin(){
+        setStandarJumlah(2);
+        setStandar_baik(2);
+        setStandarPosisi("atap");
+        System.out.println("Nama Barang: Kipas angin");
+        input();
+        System.out.println("**********************************************");
+        show();
+        
+    }
+
+    
+}
