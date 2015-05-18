40a1235637f708c88c4060b58ff1aaaaf38e0008
Dnurmasari <nurmasari.np@gmail.com>
2015-05-18 09:03:41 +0700

    Merge origin/master

--- src/tbinventarisjava/polimer.java
+++ src/tbinventarisjava/polimer.java
@@ -22,7 +22,32 @@ public class polimer {
         void posisi(){
             System.out.println("belakang");
         }
-        
+    void  kondisi(){
+        System.out.println("Kondisi");
+    }
+    class baik extends polimer{
+        void kondisi(){
+            System.out.println("sesuai");
+        }
+    class buruk extends polimer{
+        void kondisi(){
+            System.out.println("tidak sesuai");
+        }
+    }
+    void jumlah(){
+        System.out.println("jumlah");
+    }
+    class sesuai extends polimer{
+        void jumlah(){
+            System.out.println("sesuai");
+        }
+    }
+    class tidak_sesuai extends polimer{
+        void jumlah(){
+            System.out.println("Tidak sesuai");
+        }
+    }
+    }    
     }
     
 }
