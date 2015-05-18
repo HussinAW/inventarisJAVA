1d94f4e8f1f202c4c7a26f63ebaaf5eff26c2f3c
HussinAW <hussin_agung@yahoo.com>
2015-05-18 11:38:57 +0700

    new commit

--- src/tbinventarisjava/Jendela.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class Jendela extends Lingkungan_ruang{
-    public void jendela(){  
-        System.out.println("Jendela");
-        input();
-        show();
-        System.out.println("----------------------------------------------------");
-    }
-}
--- src/tbinventarisjava/Pintu.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class Pintu extends Lingkungan_ruang{
-    public void pintu(){  
-        System.out.println("Pintu");
-        input();
-        show();
-        System.out.println("----------------------------------------------------");
-    }
-}
