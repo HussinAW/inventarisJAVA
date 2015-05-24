7841ad5bf3b83b2fa9e47d9158de29a5c4205f97
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:09:36 +0700

    Delete pintu.java

--- src/tbinventarisjava/pintu.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class pintu extends Lingkungan_ruang{
-    public void pintu(){  
-        System.out.println("pintu");
-        input();
-        show();
-        
-    }
-}
