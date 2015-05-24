dab1e42d7ebd61e7f2d972d17e1883d5fab15a31
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:09:06 +0700

    Delete dinding.java

--- src/tbinventarisjava/dinding.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class dinding extends Lingkungan_ruang{
-    public void dinding(){  
-        System.out.println("dinding");
-        input();
-        show();
-        
-    }
-}
