df9fd45e12d976c5881a28bbcfd0355a80b4a302
HussinAW <hussin_agung@yahoo.com>
2015-05-24 13:59:18 +0700

    new commit

---
+++ src/tbinventarisjava/induk_LRK.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbinventarisjava;
+
+/**
+ *
+ * @author Toshiba
+ */
+
+public abstract class induk_LRK {
+ public abstract double Analisiskebersihanlantai();
+ public abstract double Analisiskebersihandinding();
+public abstract double Analisiskebersihanatap();
+public abstract double Analisiskebersihanpintu();
+public abstract double Analisiskebersihanjendela();
+}
