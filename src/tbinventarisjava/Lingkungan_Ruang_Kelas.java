66a97e011c7b0be823b4e9818bd912bcfc65b635
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:04:40 +0700

    new commit

---
+++ src/tbinventarisjava/Lingkungan_Ruang_Kelas.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbinventarisjava;
+
+/**
+ *
+ * @author hussin agung wijaya
+ */
+public class Lingkungan_Ruang_Kelas {
+    private String kondisi_lantai;
+    private String kondisi_dinding;
+    private String kondisi_atap;
+    private String kondisi_pintu;
+    private String kondisi_jendela;
+
+    /**
+     * @return the kondisi_lantai
+     */
+    public String getKondisi_lantai() {
+        return kondisi_lantai;
+    }
+
+    
+    /**
+     * @param kondisi_lantai the kondisi_lantai to set
+     */
+    public void setKondisi_lantai(String kondisi_lantai) {
+        this.kondisi_lantai = kondisi_lantai;
+    }
+
+    /**
+     * @return the kondisi_dinding
+     */
+    public String getKondisi_dinding() {
+        return kondisi_dinding;
+    }
+
+    /**
+     * @param kondisi_dinding the kondisi_dinding to set
+     */
+    public void setKondisi_dinding(String kondisi_dinding) {
+        this.kondisi_dinding = kondisi_dinding;
+    }
+
+    /**
+     * @return the kondisi_atap
+     */
+    public String getKondisi_atap() {
+        return kondisi_atap;
+    }
+
+    /**
+     * @param kondisi_atap the kondisi_atap to set
+     */
+    public void setKondisi_atap(String kondisi_atap) {
+        this.kondisi_atap = kondisi_atap;
+    }
+
+    /**
+     * @return the kondisi_pintu
+     */
+    public String getKondisi_pintu() {
+        return kondisi_pintu;
+    }
+
+    /**
+     * @param kondisi_pintu the kondisi_pintu to set
+     */
+    public void setKondisi_pintu(String kondisi_pintu) {
+        this.kondisi_pintu = kondisi_pintu;
+    }
+
+    /**
+     * @return the kondisi_jendela
+     */
+    public String getKondisi_jendela() {
+        return kondisi_jendela;
+    }
+
+    /**
+     * @param kondisi_jendela the kondisi_jendela to set
+     */
+    public void setKondisi_jendela(String kondisi_jendela) {
+        this.kondisi_jendela = kondisi_jendela;
+    }
+}
+ 
