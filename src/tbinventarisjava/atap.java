6e5493d539996cea8171913e1867e50b7bb279a3
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:08:31 +0700

    Delete atap.java

--- src/tbinventarisjava/atap.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class atap extends Lingkungan_ruang{
-    public void atap(){  
-        System.out.println("atap");
-        input();
-        show();
-        
-    }
-}
