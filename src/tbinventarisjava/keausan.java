4d057d14ec5157e4ca00611ceaa340146140bd87
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:03:17 +0700

    Delete keausan.java

--- src/tbinventarisjava/keausan.java
+++
@@ -1,8 +0,0 @@
-package tbinventarisjava;
-public class keausan extends Kenyamanan_ruang{
-    public void Keausan(){
-        System.out.println("Keausan:");
-        input();
-        show();
-    }
-}
