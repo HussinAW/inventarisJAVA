2fad6d519357358d5937f4a5ee4a2d255505754e
HussinAW <hussin_agung@yahoo.com>
2015-05-24 14:09:55 +0700

    Delete jendela.java

--- src/tbinventarisjava/jendela.java
+++
@@ -1,9 +0,0 @@
-package tbinventarisjava;
-public class jendela extends Kebersihan_ruang{
-    public void jendela(){  
-        System.out.println("jendela");
-        input();
-        show();
-        
-    }
-}
