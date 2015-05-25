de475068f5ef1c78c517a4464b8c7b2b82f5a4f4
HussinAW <hussin_agung@yahoo.com>
2015-05-25 09:54:56 +0700

    new commit

---
+++ src/tbinventarisjava/keamanan_ruang_kelas.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tbinventarisjava;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class keamanan_ruang_kelas {
+    private String kekokohan;
+    private String kunci;
+    private String keamanan;
+    private double sesuai=0.0;
+
+    /**
+     * @return the kekokohan
+     */
+    public String getKekokohan() {
+        return kekokohan;
+    }
+
+    /**
+     * @param kekokohan the kekokohan to set
+     */
+    public void setKekokohan(String kekokohan) {
+        this.kekokohan = kekokohan;
+    }
+
+    /**
+     * @return the kunci
+     */
+    public String getKunci() {
+        return kunci;
+    }
+
+    /**
+     * @param kunci the kunci to set
+     */
+    public void setKunci(String kunci) {
+        this.kunci = kunci;
+    }
+
+    /**
+     * @return the keamanan
+     */
+    public String getKeamanan() {
+        return keamanan;
+    }
+
+    /**
+     * @param keamanan the keamanan to set
+     */
+    public void setKeamanan(String keamanan) {
+        this.keamanan = keamanan;
+    }
+
+    /**
+     * @return the sesuai
+     */
+    public double getSesuai() {
+        return sesuai;
+    }
+
+    /**
+     * @param sesuai the sesuai to set
+     */
+    public void setSesuai(double sesuai) {
+        this.sesuai = sesuai;
+    }
+    
+    
+}
