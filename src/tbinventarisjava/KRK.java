22691fa9da58ebb0900fb7a6c39a37450b029f0f
HussinAW <hussin_agung@yahoo.com>
2015-06-02 09:05:10 +0700

    new commit

--- src/tbinventarisjava/KRK.java
+++ src/tbinventarisjava/KRK.java
@@ -14,18 +14,8 @@ public class KRK extends super_abstrack{
 Scanner input = new Scanner(System.in);
 public Kenyamanan_Ruang_Kelas nyaman = new Kenyamanan_Ruang_Kelas();
 public double sesuai=0.0;
-public void input(){
-    System.out.println("Masukkan bising atau tidak Ruang kelas : ");
-    nyaman.setKebisingan(input.next());
-    System.out.println("Masukkan bau atau tidak Ruang kelas :");
-    nyaman.setBau(input.next());
-    System.out.println("Masukkan bocor atau tidak Ruang kelas :");
-    nyaman.setKebocoran(input.next());
-    System.out.println("Masukkan rusak atau tidak Ruang kelas : ");
-    nyaman.setKerusakan(input.next());
-    System.out.println("Masukkan aus atau tidak Ruang kelas : ");
-    nyaman.setKeausan(input.next());
-}
+
+
 
    
 
@@ -69,6 +59,21 @@ public void input(){
     public double tampil() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public double input() {
+    System.out.println("Masukkan bising atau tidak Ruang kelas : ");
+    nyaman.setKebisingan(input.next());
+    System.out.println("Masukkan bau atau tidak Ruang kelas :");
+    nyaman.setBau(input.next());
+    System.out.println("Masukkan bocor atau tidak Ruang kelas :");
+    nyaman.setKebocoran(input.next());
+    System.out.println("Masukkan rusak atau tidak Ruang kelas : ");
+    nyaman.setKerusakan(input.next());
+    System.out.println("Masukkan aus atau tidak Ruang kelas : ");
+    nyaman.setKeausan(input.next());
+    return 0;
+}
 
 }
    
