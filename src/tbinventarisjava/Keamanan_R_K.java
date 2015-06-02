56b9a26f05e57ca73be83dd4cb6b63195681d1a7
HussinAW <hussin_agung@yahoo.com>
2015-06-02 09:06:38 +0700

    new commit

--- src/tbinventarisjava/Keamanan_R_K.java
+++ src/tbinventarisjava/Keamanan_R_K.java
@@ -16,19 +16,7 @@ Scanner input = new Scanner (System.in);
 public keamanan_ruang_kelas keamanan = new keamanan_ruang_kelas();
 public double sesuai=0.0; 
 
-public void input(){
-    System.out.println("Masukkan kondisi kekokohan: ");
-    keamanan.setKekokohan(input.next());
-    System.out.println("========================================================================/n");
-    
-     System.out.println("Masukkan kondisi kunci pintu dan jendela: ");
-    keamanan.setKunci(input.next());
-    System.out.println("========================================================================/n");
-    
-     System.out.println("Masukkan kondisi keamanan: ");
-    keamanan.setKeamanan(input.next());
-    System.out.println("========================================================================/n");
-}
+
 
     @Override
      public double Analisis() {
@@ -58,4 +46,20 @@ public void input(){
     public double tampil() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public double input() {
+       
+    System.out.println("Masukkan kondisi kekokohan: ");
+    keamanan.setKekokohan(input.next());
+    System.out.println("========================================================================/n");
+    
+     System.out.println("Masukkan kondisi kunci pintu dan jendela: ");
+    keamanan.setKunci(input.next());
+    System.out.println("========================================================================/n");
+    
+     System.out.println("Masukkan kondisi keamanan: ");
+    keamanan.setKeamanan(input.next());
+    System.out.println("========================================================================/n");
+}
 }
