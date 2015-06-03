2e16a2928f7080bf0ad454b28d33b3186113efba
HussinAW <hussin_agung@yahoo.com>
2015-06-03 12:47:12 +0700

    new commit

--- src/tbinventarisjava/Pemanggilan.java
+++ src/tbinventarisjava/Pemanggilan.java
@@ -3,17 +3,29 @@ public class Pemanggilan {
  
     
     public static void main(String[]Args){
+        IR as= new IR();
+        as.input();
+        as.simpan();
         super_abstrack elektronik = new JKPS();
         elektronik.input();
+        JKPS ag = new JKPS();
+        ag.simpan();
         super_abstrack lingkungan = new  LRK();
         lingkungan.input();
+        LRK ad = new LRK();
+        ad.simpan();
         super_abstrack kenyamanan = new KRK();
         kenyamanan.input();
+        KRK ah = new KRK();
+        ah.simpan();
         super_abstrack keamanan= new Keamanan_R_K();
         keamanan.input();
+        Keamanan_R_K aj = new Keamanan_R_K();
+        aj.simpan();
         
         
         
+        as.tampil();
         super_abstrack jay = new JKPS();
         jay.tampil();
         super_abstrack dwi = new LRK();
