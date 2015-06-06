f50b05008ace2a703e4100ddee7dc3c97d27a8a9
HussinAW <hussin_agung@yahoo.com>
2015-06-06 11:28:19 +0700

    new commit

--- src/tbinventarisjava/IR.java
+++ src/tbinventarisjava/IR.java
@@ -15,7 +15,7 @@ public class IR{
     Scanner input = new Scanner(System.in);
     Identitas_ruang IR = new Identitas_ruang();
     public double sesuai = 0.0;
-    public double input(){
+    /*public double input(){
         System.out.println("Nama ruang:\t");
         IR.setNama_ruang(input.next());
         System.out.println("=============================================");
@@ -27,26 +27,7 @@ public class IR{
         System.out.println("=============================================");
         return 0;
     }
-    
-    public void simpan(){
-        try{
-            FileWriter Writer = new FileWriter("Identitas Kelas.txt");
-            Writer.write("Nama ruang= "+IR.getNama_ruang()+"\t");
-            Writer.write("Lokasi ruang= "+IR.getLokasi_ruang()+"\t");
-            Writer.write("Program studi= "+IR.getProgram_studi()+"\t"); 
-            Writer.close();
-        }
-        catch(IOException ex){
-            ex.printStackTrace();
-            
-        }
-    }
-    
-    public double tampil(){
-        System.out.println("Nama ruang:\t"+IR.getNama_ruang());
-        System.out.println("Lokasi ruang:\t"+IR.getLokasi_ruang());
-        System.out.println("Program studi:\t"+IR.getProgram_studi());
-        return 0;
-    }
+    */
+
     
 }
